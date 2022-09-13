73b736f8551fbb4ab0902805b07cb58e0f7e0c5c
Pethe <pethersonreis@gmail.com>
2022-09-13 10:00:13 -0300

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { navbar as NavBar } from "./components/navbar/navbar";
+
+import "./index.css";
+
+function App() {
+	return (
+		<div className="App">
+			<NavBar />
+		</div>
+	);
+}
+
+export default App;
